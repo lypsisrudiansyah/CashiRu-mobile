6654fe0c1c31df508540a1f914dcfb23a8d5472f
rudiansyah <rudiansyahether@gmail.com>
2025-07-15 10:43:13 +0700

    ✨ (core): add new constants and extensions feat(core): add new constants and extensions ✨ (core): add new constants and extensions feat(core): add new constants and extensions ⬆️ (pubspec.yaml): upgrade dependencies

---
+++ lib/core/constants/variables.dart
@@ -0,0 +1,6 @@
+class Variables {
+  // static const String baseUrl = 'http://192.168.18.49:8000';
+  static const String baseUrl = 'http://192.168.0.113:8000';
+  // http://192.168.1.101:8000
+  static const String imageBaseUrl = '$baseUrl/storage/';
+}
