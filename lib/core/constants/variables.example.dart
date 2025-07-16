e5ef835e124894363482938a65ce0ba8eae0756a
rudiansyah <rudiansyahether@gmail.com>
2025-07-16 15:11:55 +0700

    ✨ (core): add variables.example.dart file with base url and http client 🔧 (gitignore): add /lib/core/constants/variables.dart to gitignore file

---
+++ lib/core/constants/variables.example.dart
@@ -0,0 +1,9 @@
+import 'package:http/http.dart' as http;
+
+class Variables {
+  static const String baseUrl = 'http://localhost:8000';
+  // static const String baseUrl = 'http://192.168.0.113:8000';
+  static const String imageBaseUrl = '$baseUrl/storage/';
+
+  static http.Client httpClient = http.Client();
+}
