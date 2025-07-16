3ce01327bd4dbea252a3788dbea4c787f5107df0
rudiansyah <rudiansyahether@gmail.com>
2025-07-16 20:55:30 +0700

    ✨ (test): add product and report remote data sources to fetch data

---
+++ test/unit/data/datasource/report_remote_datasource.dart
@@ -0,0 +1,38 @@
+import 'package:cashiru/core/constants/variables.dart';
+import 'package:cashiru/data/datasource/auth_local_datasource.dart';
+import 'package:cashiru/data/models/response/product_sales_response_model.dart';
+import 'package:cashiru/data/models/response/summary_response_model.dart';
+import 'package:dartz/dartz.dart';
+import 'package:http/http.dart' as http;
+
+class ReportRemoteDatasource {
+  final AuthLocalDatasource _localDatasource;
+  ReportRemoteDatasource({AuthLocalDatasource? localDatasource}) : _localDatasource = localDatasource ?? AuthLocalDatasource();
+
+  Future<Either<String, SummaryResponseModel>> getSummaryReport(String startDate, String endDate) async {
+    final authData = await _localDatasource.getAuthData();
+    final response = await Variables.httpClient.get(
+      Uri.parse('${Variables.baseUrl}/api/reports/summary?start_date=$startDate&end_date=$endDate'),
+      headers: {'Authorization': 'Bearer ${authData?.token}'},
+    );
+    if (response.statusCode == 200) {
+      return right(SummaryResponseModel.fromJson(response.body));
+    } else {
+      return left(response.body);
+    }
+  }
+
+  Future<Either<String, ProductSalesResponseModel>> getProductSales(String startDate, String endDate) async {
+    final authData = await _localDatasource.getAuthData();
+    final response = await Variables.httpClient.get(
+      Uri.parse('${Variables.baseUrl}/api/reports/product-sales?start_date=$startDate&end_date=$endDate'),
+      headers: {'Authorization': 'Bearer ${authData?.token}', 'Content-Type': 'application/json'},
+    );
+
+    if (response.statusCode == 200) {
+      return right(ProductSalesResponseModel.fromJson(response.body));
+    } else {
+      return left(response.body);
+    }
+  }
+}
