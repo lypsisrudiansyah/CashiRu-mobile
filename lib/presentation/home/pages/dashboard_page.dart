243c566fd844c9590aca9e693869c2948065f782
rudiansyah <rudiansyahether@gmail.com>
2025-07-18 10:00:10 +0700

    ✨ (dashboard_page.dart): implement bottom navigation bar ✨ (dashboard_page.dart): add navigation items ✨ (dashboard_page.dart): add navigation bar design ✨ (dashboard_page.dart): add navigation to home page

--- lib/presentation/home/pages/dashboard_page.dart
+++ lib/presentation/home/pages/dashboard_page.dart
@@ -1,3 +1,4 @@
+import 'package:cashiru/presentation/home/pages/home_page.dart';
 import 'package:flutter/material.dart';
 
 import '../../../core/assets/assets.gen.dart';
@@ -29,6 +30,55 @@ class _DashboardPageState extends State<DashboardPage> {
 
   @override
   Widget build(BuildContext context) {
-
+    return Scaffold(
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: Container(
+        padding: EdgeInsets.all(20.0),
+        decoration: BoxDecoration(
+          // * Try This
+          borderRadius: BorderRadius.vertical(top: Radius.circular(30)),
+          color: AppColors.white,
+          boxShadow: [
+            // * Try This
+            BoxShadow(
+              offset: Offset(0, -2),
+              blurRadius: 30.0,
+              blurStyle: BlurStyle.outer,
+              spreadRadius: 0,
+              color: AppColors.black.withOpacity(0.08),
+            ),
+          ],
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            NavItem(
+              iconPath: Assets.icons.home.path,
+              label: 'Home',
+              isActive: _selectedIndex == 0,
+              onTap: () => _onItemTapped(0),
+            ),
+            NavItem(
+              iconPath: Assets.icons.orders.path,
+              label: 'Orders',
+              isActive: _selectedIndex == 1,
+              onTap: () => _onItemTapped(1),
+            ),
+            NavItem(
+              iconPath: Assets.icons.payments.path,
+              label: 'History',
+              isActive: _selectedIndex == 2,
+              onTap: () => _onItemTapped(2),
+            ),
+            NavItem(
+              iconPath: Assets.icons.dashboard.path,
+              label: 'Setting',
+              isActive: _selectedIndex == 3,
+              onTap: () => _onItemTapped(3),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
