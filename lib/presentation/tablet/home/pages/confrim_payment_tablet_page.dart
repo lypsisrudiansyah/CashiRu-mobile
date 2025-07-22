0b41596bb1fa3c126b91ed6930727fac279384f2
rudiansyah <rudiansyahether@gmail.com>
2025-07-22 22:18:40 +0700

    ✨ (tablet): implement order items list on confirm payment page ✨ (tablet): add dummy order items data to confirm payment page ✨ (tablet): implement total calculation on confirm payment page ✨ (tablet): implement UI for payment options on confirm payment page ✨ (tablet): implement UI for money input on confirm payment page
    ✨ (manage_printer_tablet_page.dart): create manage printer page for tablet
    ✨ (manage_printer_tablet_page.dart): implement bluetooth printer connection
    ✨ (manage_printer_tablet_page.dart): implement printer search and test
    ✨ (manage_printer_tablet_page.dart): add UI for printer management

--- lib/presentation/tablet/home/pages/confrim_payment_tablet_page.dart
+++ lib/presentation/tablet/home/pages/confrim_payment_tablet_page.dart
@@ -1,6 +1,9 @@
 import 'dart:developer';
 
 import 'package:cashiru/data/models/response/auth_response_model.dart';
+import 'package:cashiru/data/models/response/category_response_model.dart';
+import 'package:cashiru/data/models/response/product_response_model.dart';
+import 'package:cashiru/presentation/home/models/order_item.dart';
 import 'package:flutter/material.dart';
 import 'package:cashiru/core/components/buttons.dart';
 import 'package:cashiru/core/components/spaces.dart';
@@ -9,13 +12,11 @@ import 'package:cashiru/core/extensions/build_context_extension.dart';
 import 'package:cashiru/core/extensions/int_extension.dart';
 import 'package:cashiru/presentation/tablet/home/widgets/order_menu.dart';
 
-
 class ConfrimPaymentTabletPage extends StatefulWidget {
   const ConfrimPaymentTabletPage({super.key});
 
   @override
-  State<ConfrimPaymentTabletPage> createState() =>
-      _ConfrimPaymentTabletPageState();
+  State<ConfrimPaymentTabletPage> createState() => _ConfrimPaymentTabletPageState();
 }
 
 class _ConfrimPaymentTabletPageState extends State<ConfrimPaymentTabletPage> {
@@ -31,6 +32,39 @@ class _ConfrimPaymentTabletPageState extends State<ConfrimPaymentTabletPage> {
   }
 
   loadData() async {}
+
+  List<OrderItem> dummyOrderItems = [
+    OrderItem(
+      product: Product(
+        id: 1,
+
+        name: "Nasi Goreng Spesial",
+        price: '25000',
+        category: Category(id: 1, name: "Makanan Berat"),
+        image: "...",
+        stock: 100, // Tambahkan stock untuk draft
+        categoryId: 1, // Tambahkan categoryId untuk draft
+        createdAt: DateTime.now(), // Tambahkan createdAt untuk draft
+        updatedAt: DateTime.now(), // Tambahkan updatedAt untuk draft
+      ),
+      quantity: 2,
+    ),
+    OrderItem(
+      product: Product(
+        id: 2,
+        // productId: 102,
+        name: "Sate Ayam Madura",
+        price: '30000',
+        category: Category(id: 1, name: "Makanan Berat"),
+        image: "...",
+        stock: 100, // Tambahkan stock untuk draft
+        categoryId: 1, // Tambahkan categoryId untuk draft
+        createdAt: DateTime.now(), // Tambahkan createdAt untuk draft
+        updatedAt: DateTime.now(), // Tambahkan updatedAt untuk draft
+      ),
+      quantity: 1,
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -67,10 +101,7 @@ class _ConfrimPaymentTabletPageState extends State<ConfrimPaymentTabletPage> {
                                 ),
                                 Text(
                                   'Orders #',
-                                  style: TextStyle(
-                                    fontSize: 16,
-                                    fontWeight: FontWeight.w500,
-                                  ),
+                                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
                                 ),
                               ],
                             ),
@@ -90,9 +121,7 @@ class _ConfrimPaymentTabletPageState extends State<ConfrimPaymentTabletPage> {
                                 fontWeight: FontWeight.w600,
                               ),
                             ),
-                            SizedBox(
-                              width: 160,
-                            ),
+                            SizedBox(width: 160),
                             SizedBox(
                               width: 50.0,
                               child: Text(
@@ -120,15 +149,13 @@ class _ConfrimPaymentTabletPageState extends State<ConfrimPaymentTabletPage> {
                         const Divider(),
                         const SpaceHeight(8),
                         // * Try This - Should Be Implement
-                        // ListView.separated(
-                        //   shrinkWrap: true,
-                        //   physics: const NeverScrollableScrollPhysics(),
-                        //   itemBuilder: (context, index) =>
-                        //       OrderMenu(data: dummyOrderItems[index]),
-                        //   separatorBuilder: (context, index) =>
-                        //       const SpaceHeight(16.0),
-                        //   itemCount: dummyOrderItems.length,
-                        // ),
+                        ListView.separated(
+                          shrinkWrap: true,
+                          physics: const NeverScrollableScrollPhysics(),
+                          itemBuilder: (context, index) => OrderMenu(data: dummyOrderItems[index]),
+                          separatorBuilder: (context, index) => const SpaceHeight(16.0),
+                          itemCount: dummyOrderItems.length,
+                        ),
                         const SpaceHeight(16.0),
                         const SpaceHeight(8.0),
                         const Divider(),
@@ -136,65 +163,51 @@ class _ConfrimPaymentTabletPageState extends State<ConfrimPaymentTabletPage> {
                         Column(
                           children: [
                             const Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Text(
-                                    'Subtotal',
-                                    style: TextStyle(color: AppColors.grey),
-                                  ),
-                                  // Text(
-                                  //   subTotal.currencyFormatRp,
-                                  //   style: const TextStyle(
-                                  //     color: AppColors.primary,
-                                  //     fontWeight: FontWeight.w600,
-                                  //   ),
-                                  // ),
-                                ]),
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text('Subtotal', style: TextStyle(color: AppColors.grey)),
+                                // Text(
+                                //   subTotal.currencyFormatRp,
+                                //   style: const TextStyle(
+                                //     color: AppColors.primary,
+                                //     fontWeight: FontWeight.w600,
+                                //   ),
+                                // ),
+                              ],
+                            ),
                             const SpaceHeight(8.0),
                             const Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Text(
-                                    'Discount',
-                                    style: TextStyle(color: AppColors.grey),
-                                  ),
-                                ]),
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [Text('Discount', style: TextStyle(color: AppColors.grey))],
+                            ),
                             const SpaceHeight(8.0),
                             Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  const Text(
-                                    'Tax',
-                                    style: TextStyle(color: AppColors.grey),
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                const Text('Tax', style: TextStyle(color: AppColors.grey)),
+                                Text(
+                                  10000.currencyFormatRp,
+                                  style: const TextStyle(
+                                    color: AppColors.primary,
+                                    fontWeight: FontWeight.w600,
                                   ),
-                                  Text(
-                                    10000.currencyFormatRp,
-                                    style: const TextStyle(
-                                      color: AppColors.primary,
-                                      fontWeight: FontWeight.w600,
-                                    ),
-                                  ),
-                                ]),
+                                ),
+                              ],
+                            ),
                             const SpaceHeight(8.0),
                             const Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Text(
-                                    'Service Charge',
-                                    style: TextStyle(color: AppColors.grey),
-                                  ),
-                                  // Text(
-                                  //   finalService.currencyFormatRp,
-                                  //   style: const TextStyle(
-                                  //     color: AppColors.primary,
-                                  //     fontWeight: FontWeight.w600,
-                                  //   ),
-                                  // ),
-                                ]),
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text('Service Charge', style: TextStyle(color: AppColors.grey)),
+                                // Text(
+                                //   finalService.currencyFormatRp,
+                                //   style: const TextStyle(
+                                //     color: AppColors.primary,
+                                //     fontWeight: FontWeight.w600,
+                                //   ),
+                                // ),
+                              ],
+                            ),
                             const SpaceHeight(12.0),
                             Row(
                               mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -202,8 +215,9 @@ class _ConfrimPaymentTabletPageState extends State<ConfrimPaymentTabletPage> {
                                 const Text(
                                   'Total',
                                   style: TextStyle(
-                                      color: AppColors.primary,
-                                      fontWeight: FontWeight.w600),
+                                    color: AppColors.primary,
+                                    fontWeight: FontWeight.w600,
+                                  ),
                                 ),
                                 Text(
                                   total.currencyFormatRp,
@@ -211,9 +225,9 @@ class _ConfrimPaymentTabletPageState extends State<ConfrimPaymentTabletPage> {
                                     color: AppColors.primary,
                                     fontWeight: FontWeight.bold,
                                   ),
-                                )
+                                ),
                               ],
-                            )
+                            ),
                           ],
                         ),
                       ],
@@ -244,10 +258,7 @@ class _ConfrimPaymentTabletPageState extends State<ConfrimPaymentTabletPage> {
                             ),
                             const Text(
                               '2 payment option available',
-                              style: TextStyle(
-                                fontSize: 16,
-                                fontWeight: FontWeight.w500,
-                              ),
+                              style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
                             ),
                             const SpaceHeight(8.0),
                             const Divider(),
@@ -330,14 +341,12 @@ class _ConfrimPaymentTabletPageState extends State<ConfrimPaymentTabletPage> {
                             const SpaceHeight(45.0),
                             Wrap(
                               spacing: 20.0, // horizontal spacing each widget
-                              runSpacing:
-                                  10.0, // vertical spacing each lines/rows of widgets
+                              runSpacing: 10.0, // vertical spacing each lines/rows of widgets
                               children: [
                                 Button.filled(
                                   width: 150.0,
                                   onPressed: () {
-                                    totalController.text =
-                                        total.currencyFormatRp;
+                                    totalController.text = total.currencyFormatRp;
                                     log("totalController.text: ${totalController.text}");
                                   },
                                   label: 'Money',
@@ -345,49 +354,41 @@ class _ConfrimPaymentTabletPageState extends State<ConfrimPaymentTabletPage> {
                                 Button.filled(
                                   width: 150.0,
                                   onPressed: () {
-                                    totalController.text =
-                                        10000.currencyFormatRp;
+                                    totalController.text = 10000.currencyFormatRp;
                                   },
                                   label: 'Rp 10.000',
                                 ),
                                 Button.filled(
                                   width: 150.0,
                                   onPressed: () {
-                                    totalController.text =
-                                        20000.currencyFormatRp;
+                                    totalController.text = 20000.currencyFormatRp;
                                   },
                                   label: 'Rp 20.000',
                                 ),
                                 Button.filled(
                                   width: 150.0,
                                   onPressed: () {
-                                    totalController.text =
-                                        50000.currencyFormatRp;
+                                    totalController.text = 50000.currencyFormatRp;
                                   },
                                   label: 'Rp 50.000',
                                 ),
                                 Button.filled(
                                   width: 150.0,
                                   onPressed: () {
-                                    totalController.text =
-                                        75000.currencyFormatRp;
+                                    totalController.text = 75000.currencyFormatRp;
                                   },
                                   label: 'Rp 75.000',
                                 ),
-                                ...List.generate(
-                                  10,
-                                  (index) {
-                                    final nominal = (index + 1) * 100000;
-                                    return Button.filled(
-                                      width: 150.0,
-                                      onPressed: () {
-                                        totalController.text =
-                                            nominal.currencyFormatRp;
-                                      },
-                                      label: nominal.currencyFormatRp,
-                                    );
-                                  },
-                                ),
+                                ...List.generate(10, (index) {
+                                  final nominal = (index + 1) * 100000;
+                                  return Button.filled(
+                                    width: 150.0,
+                                    onPressed: () {
+                                      totalController.text = nominal.currencyFormatRp;
+                                    },
+                                    label: nominal.currencyFormatRp,
+                                  );
+                                }),
                               ],
                             ),
                             const SpaceHeight(100.0),
@@ -399,8 +400,7 @@ class _ConfrimPaymentTabletPageState extends State<ConfrimPaymentTabletPage> {
                         child: ColoredBox(
                           color: AppColors.white,
                           child: Padding(
-                            padding: const EdgeInsets.symmetric(
-                                horizontal: 24.0, vertical: 16.0),
+                            padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 16.0),
                             child: Row(
                               children: [
                                 Flexible(
@@ -411,11 +411,8 @@ class _ConfrimPaymentTabletPageState extends State<ConfrimPaymentTabletPage> {
                                 ),
                                 const SpaceWidth(8.0),
                                 Flexible(
-                                  child: Button.filled(
-                                    onPressed: () async {},
-                                    label: 'Pay',
-                                  ),
-                                )
+                                  child: Button.filled(onPressed: () async {}, label: 'Pay'),
+                                ),
                               ],
                             ),
                           ),
