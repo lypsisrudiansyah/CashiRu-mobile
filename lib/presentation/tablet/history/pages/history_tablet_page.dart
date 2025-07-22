6870a5dfbeae33faf5cc80266790a84daa1c9a5a
rudiansyah <rudiansyahether@gmail.com>
2025-07-22 21:39:36 +0700

    ♻️ (history_tablet_page.dart): remove unused import and variable to improve code quality

--- lib/presentation/tablet/history/pages/history_tablet_page.dart
+++ lib/presentation/tablet/history/pages/history_tablet_page.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:cashiru/core/components/spaces.dart';
 import 'package:cashiru/core/constants/colors.dart';
-import 'package:cashiru/presentation/history/widgets/history_transaction_card.dart';
 
 class HistoryTabletPage extends StatefulWidget {
   const HistoryTabletPage({super.key});
@@ -18,7 +16,7 @@ class _HistoryTabletPageState extends State<HistoryTabletPage> {
 
   @override
   Widget build(BuildContext context) {
-    const paddingHorizontal = EdgeInsets.symmetric(horizontal: 16.0);
+    // const paddingHorizontal = EdgeInsets.symmetric(horizontal: 16.0);
 
     return Scaffold(
       body: Padding(
